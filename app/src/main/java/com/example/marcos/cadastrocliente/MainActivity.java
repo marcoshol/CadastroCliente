57b866247fbe50843b45b1d5ade83b123f1a3628
Marcos <marcos.holanda10@gmail.com>
2015-05-03 19:25:34 -0300

    BancoDeDados

---
+++ app/src/main/java/com/example/marcos/cadastrocliente/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.marcos.cadastrocliente;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    EditText nome, cidade;
+    String snome, scidade;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        nome = (EditText) findViewById(R.id.edt_nome);
+        cidade = (EditText) findViewById(R.id.edt_cidade);
+        snome = nome.getText().toString();
+        scidade = cidade.getText().toString();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
